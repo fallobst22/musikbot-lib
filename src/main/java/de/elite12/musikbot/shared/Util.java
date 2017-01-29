40ee19eda397940852a9a53ff664f3a879c4f234
svkir <svkir@DESKTOP-5E9PKP8>
2017-01-29 16:33:26 +0100

    #MUS-1 #Mus-67 #resolved

--- src/main/java/de/elite12/musikbot/shared/Util.java
+++ src/main/java/de/elite12/musikbot/shared/Util.java
@@ -9,6 +9,9 @@ import java.util.TimerTask;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 import org.apache.log4j.Logger;
 
 import com.wrapper.spotify.Api;
@@ -293,4 +296,15 @@ public class Util {
         }
         return null;
     }
+
+	public static boolean isValidEmailAddress(String email) {
+		boolean result = true;
+		try {
+			InternetAddress emailAddr = new InternetAddress(email);
+			emailAddr.validate();
+		} catch (AddressException ex) {
+			result = false;
+		}
+		return result;
+	}
 }
